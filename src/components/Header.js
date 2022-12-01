610bd4c580fdd2d8905487d5c236e2422c2eadbc
EbukaNweje <nwejeebuka@gmail.com>
2022-12-01 10:51:06 +0100

    Bukka Food

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "../css/mobile.css"
+
+const Header = () => {
+  return (
+    <div className="Header">
+        <div className='Logo'></div>
+        <div className='SreachIcon'>
+            <input type="text" placeholder="What on your mind?"/>
+        </div>
+        <div>c</div>
+    </div>
+  )
+}
+
+export default Header
